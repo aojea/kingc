4166efa962c4e8e81aea94ddc804617d358f52cc
Antonio Ojea <aojea@google.com>
2026-01-28 22:29:25 +0000

    fix kubeconfig generation

--- pkg/config/kubeconfig.go
+++ pkg/config/kubeconfig.go
@@ -6,7 +6,8 @@ import (
 )
 
 // GenerateKubeconfig creates a kubeconfig content string
-func GenerateKubeconfig(clusterName, serverURL string, caCert, clientCert, clientKey []byte) string {
+// GenerateKubeconfig creates a kubeconfig content string
+func GenerateKubeconfig(clusterName, serverURL, userName string, caCert, clientCert, clientKey []byte) string {
 	return fmt.Sprintf(`apiVersion: v1
 clusters:
 - cluster:
@@ -16,25 +17,29 @@ clusters:
 contexts:
 - context:
     cluster: %s
-    user: kubernetes-admin
-  name: kubernetes-admin@%s
-current-context: kubernetes-admin@%s
+    user: %s
+  name: %s@%s
+current-context: %s@%s
 kind: Config
 preferences: {}
 users:
-- name: kubernetes-admin
+- name: %s
   user:
     client-certificate-data: %s
     client-key-data: %s
 `,
-		base64Encode(caCert),
-		serverURL,
+		base64Encode(caCert), // cluster.certificate-authority-data
+		serverURL,            // cluster.server
+		clusterName,          // cluster.name
+		clusterName,          // context.cluster
+		userName,             // context.user
+		userName,             // context.name (user@cluster)
 		clusterName,
+		userName, // current-context (user@cluster)
 		clusterName,
-		clusterName,
-		clusterName,
-		base64Encode(clientCert),
-		base64Encode(clientKey),
+		userName,                 // users.name
+		base64Encode(clientCert), // user.client-certificate-data
+		base64Encode(clientKey),  // user.client-key-data
 	)
 }
 
