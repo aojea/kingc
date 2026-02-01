03b3d332f98484f20098583c805609a1bfea2db4
Antonio Ojea <aojea@google.com>
2026-02-01 14:14:43 +0000

    fix repo

--- pkg/cluster/templates/startup.sh
+++ pkg/cluster/templates/startup.sh
@@ -32,9 +32,10 @@ systemctl restart containerd
 # Install Kubeadm/Kubelet/Kubectl
 # Version is injected by kingc based on config
 KUBERNETES_VERSION="{{ .KubernetesVersion }}"
+KUBERNETES_REPO_VERSION="{{ .KubernetesRepoVersion }}"
 
-curl -fsSL "https://pkgs.k8s.io/core:/stable:/${KUBERNETES_VERSION}/deb/Release.key" | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBERNETES_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL "https://pkgs.k8s.io/core:/stable:/${KUBERNETES_REPO_VERSION}/deb/Release.key" | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBERNETES_REPO_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
